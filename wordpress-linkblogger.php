77beffd43ee7ba36f0dc0f5a376a5edbf20b6c3f
jeremyfelt <jeremy.felt@gmail.com>
2012-08-17 00:47:03 -0700

    Initial commit

---
+++ wordpress-linkblogger.php
@@ -0,0 +1,54 @@
+<?php
+/*
+Plugin Name: WordPress Linkblogger
+Plugin URI: http://github.com/jeremyfelt/WordPress-Linkblogger
+Description: Allows 'retweeting' of content from supported linkblogs
+Version: 0.1
+Author: Jeremy Felt
+Author URI: http://jeremyfelt.com
+License: GPL2
+*/
+
+/*  Copyright 2012 Jeremy Felt (email: jeremy.felt@gmail.com)
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+add_action( 'init', 'jf_capture_linkblog_request' );
+function jf_capture_linkblog_request() {
+
+	if ( isset( $_SERVER['HTTP_REFERRER'] ) && '' != $_SERVER['HTTP_REFERRER'] && isset( $_GET['link'] ) && isset( $_GET['title'] ) && isset( $_GET['description'] ) )
+		$url_data = parse_url( $_SERVER['HTTP_REFERER'] );
+	else
+		return;
+
+	$valid_referral_hosts = array( 'tabs.mediahackers.org' );
+
+	if ( ! isset( $url_data['host'] ) || ! in_array( $url_data['host'], $valid_referral_hosts ) )
+		return;
+
+	$new_post_id = wp_insert_post( array(
+	                                    'post_type' => 'post',
+	                                    'post_content' => wp_kses_post( $_GET['description'] ),
+	                                    'post_title' => sanitize_title( $_GET['title'] ),
+	                                    'post_status' => 'draft',
+	                               ));
+
+	if ( is_wp_error( $new_post_id ) || 0 == absint( $new_post_id ) )
+		return;
+
+	wp_set_post_terms( $new_post_id, 'Linkblog', 'category' );
+	update_post_meta( $new_post_id, 'linkblog_url', esc_url_raw( $_GET['link'] ) );
+
+}
